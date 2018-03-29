5f7cb2e7c1189ae5c30a3e7b928ee40e35470335
pinesj <pinesj@naver.com>
2018-03-29 10:06:09 +0900

    Revert "피드백 추가중"
    This reverts commit b041e300630818b64b4cbe0606a023060ed9bbb8.

--- feedback/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Feedback(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    comment = models.TextField()
-    createDate = models.DateTimeField()
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from home import views
-# from django.contrib.auth import views
+
+from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index),
-    # url(r'^accounts/login/$', views.login, name='login'),
-    # url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
-    # url(r'', include('blog.urls')),
+    url(r'^accounts/login/$', views.login, name='login'),
+    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
+    url(r'', include('blog.urls')),
 ]
